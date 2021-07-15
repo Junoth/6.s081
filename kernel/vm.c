f1e8cb95909f568d28da05f02b62c2204a8cc2dc
junoth <heathmyh@gmail.com>
2021-07-15 06:23:41 +0000

    Lab:page tables Finish the first task to print page tables and pass test

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,33 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void 
+vmprintwalk(pagetable_t pagetable, int level) 
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V) {
+      // this PTE is valid.
+      uint64 child = PTE2PA(pte);
+      for (int j = 0; j <= level; ++j) {
+        printf("..");
+        if (j != level)
+          printf(" ");  
+      }
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        // this PTE points to a lower-level page table
+        vmprintwalk((pagetable_t)child, level + 1);
+      }
+    }   
+  }
+}
+
+void 
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprintwalk(pagetable, 0);
+}
